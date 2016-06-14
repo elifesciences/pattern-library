a375ff59d52864a54a956df664b6242a1f707aec
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-06-14 16:01:40 +0100

    Add script that validates JSON files (#187)

--- project_tests.sh
+++ project_tests.sh
@@ -3,9 +3,11 @@ set -e
 
 echo "Installing dependencies..."
 npm install
+composer install --no-interaction
 echo "Building with gulp..."
 gulp --environment production
 echo "Running tests..."
 gulp test
+bin/validate
 echo "Generating PatternLab..."
 php ./core/builder.php -g
