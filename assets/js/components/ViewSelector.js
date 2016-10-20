52c847cfc1533c8fe7d19b8f5316a8d8bc7fe05e
David Moulton <d.moulton@elifesciences.org>
2016-10-20 22:23:11 +0100

    Add tests for view-selector

--- assets/js/components/ViewSelector.js
+++ assets/js/components/ViewSelector.js
@@ -2,10 +2,8 @@
 
 module.exports = class ViewSelector {
 
-  // Passing window and document separately allows for independent mocking of window in order
-  // to test feature support fallbacks etc.
   constructor($elm, _window = window, doc = document) {
-    _window.console.log('$elm is ' + $elm);
+
     if (!$elm) {
       return;
     }
@@ -15,23 +13,18 @@ module.exports = class ViewSelector {
     this.$elm = $elm;
     this.$elmYOffset = this.$elm.offsetTop;
     this.cssFixedClassName = 'view-selector--fixed';
-
-    this.$elm.classList.add('view-selector--js');
     this.window.addEventListener('scroll', this.handleScroll.bind(this));
 
   }
 
   handleScroll() {
-    if (!this.$elm.classList.contains(this.cssFixedClassName)) {
-      if (this.window.pageYOffset >= this.$elmYOffset) {
-        this.$elm.classList.add(this.cssFixedClassName);
+    if (this.$elm.classList.contains(this.cssFixedClassName)) {
+      if (this.window.pageYOffset < this.$elmYOffset) {
+        this.$elm.classList.remove(this.cssFixedClassName);
       }
-    } else if (this.window.pageYOffset < this.$elmYOffset) {
-      this.$elm.classList.remove(this.cssFixedClassName);
+    } else if (this.window.pageYOffset >= this.$elmYOffset) {
+      this.$elm.classList.add(this.cssFixedClassName);
     }
-    // if we're currently fixed
-      // check current Y posn against scrollPosnWhenInitiallyFixed to see if neeed to unfix
   }
-
 
 };
--- test/viewselector.spec.js
+++ test/viewselector.spec.js
@@ -6,11 +6,50 @@ let ViewSelector = require('../assets/js/components/ViewSelector');
 describe('A ViewSelector Component', function () {
   'use strict';
   let $elm;
-  let viewSelector;
 
   beforeEach(function () {
     $elm = document.querySelector('[data-behaviour="ViewSelector"]');
-    viewSelector = new ViewSelector($elm);
+  });
+
+  it('uses the css class named "view-selector--fixed"', function () {
+    let viewSelector = new ViewSelector($elm);
+    expect(viewSelector.cssFixedClassName).to.equal('view-selector--fixed');
+  });
+
+  it('adds the class when sufficient scrolling has occurred', function () {
+    // Fake sufficient scrolling
+    let windowMock = {
+      addEventListener: function (){},
+      pageYOffset: 20
+    };
+    let _viewSelector1 = new ViewSelector($elm, windowMock);
+    _viewSelector1.$elmYOffset = 20;
+    _viewSelector1.handleScroll();
+
+    let classes1 = _viewSelector1.$elm.classList;
+    expect(classes1.contains('view-selector--fixed')).to.be.true;
+
+    let _viewSelector2 = new ViewSelector($elm, windowMock);
+    _viewSelector2.$elmYOffset = 20;
+    _viewSelector2.handleScroll();
+
+    let classes2 = _viewSelector2.$elm.classList;
+    expect(classes2.contains('view-selector--fixed')).to.be.true;
+  });
+
+  it('removes the class scrolling is insufficient', function () {
+    // Fake sufficient scrolling
+    let windowMock = {
+      addEventListener: function (){},
+      pageYOffset: 10
+    };
+    let _viewSelector = new ViewSelector($elm, windowMock);
+    _viewSelector.$elmYOffset = 20;
+    _viewSelector.handleScroll();
+
+    let classes = _viewSelector.$elm.classList;
+    expect(classes.contains('view-selector--fixed')).to.be.false;
+
   });
 
 });
