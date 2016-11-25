a4506da45bd5eeb1021045b36432e6b6e8df7f36
David Moulton <d.moulton@elifesciences.org>
2016-11-25 12:53:10 +0000

    Fix console errors

--- assets/js/components/ArticleSection.js
+++ assets/js/components/ArticleSection.js
@@ -68,8 +68,8 @@ module.exports = class ArticleSection {
     section.$elm.classList.remove('article-section--collapsed');
     let isHidden = section.$body.classList.contains('visuallyhidden');
     section.$body.classList.remove('visuallyhidden');
-    if (isHidden && !!this.window.MathJax && !!this.window.MathJax.Hub) {
-      this.window.MathJax.Hub.Queue(['Rerender', this.window.MathJax.Hub, section.$elm.id]);
+    if (isHidden && !!section.window.MathJax && !!section.window.MathJax.Hub) {
+      section.window.MathJax.Hub.Queue(['Rerender', section.window.MathJax.Hub, section.$elm.id]);
     }
   }
 
