8d370bd579df0108fc49dbdef09827ff1a3eb875
David Moulton <d.moulton@elifesciences.org>
2016-12-06 12:09:38 +0000

    Stop floating view-selector overlaying following elements

--- assets/js/components/ViewSelector.js
+++ assets/js/components/ViewSelector.js
@@ -22,13 +22,27 @@ module.exports = class ViewSelector {
   }
 
   handleScroll() {
+
+    // If it's position is fixed
     if (this.$elm.classList.contains(this.cssFixedClassName)) {
+
+      let bottomOfMain = this.doc.querySelector('[role="main"]').getBoundingClientRect().bottom;
+
+      // Allow it to scroll again if it could keep its position & not scroll off top of screen
       if (this.window.pageYOffset < this.elmYOffset) {
         this.$elm.classList.remove(this.cssFixedClassName);
+
+      // Allow it to scroll again if it would otherwise over-/under-lay following element
+      } else if (bottomOfMain < this.$elm.offsetHeight) {
+        let amountToNudgeUp = bottomOfMain - this.$elm.offsetHeight;
+        this.$elm.style.top = amountToNudgeUp + 'px';
       }
+
+    // Otherwise fix its position if it would otherwise scroll off the top of the screen
     } else if (this.window.pageYOffset >= this.elmYOffset) {
       this.$elm.classList.add(this.cssFixedClassName);
     }
+
   }
 
 };
