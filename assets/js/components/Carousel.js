54b790089a94d2755d0cdb3489e1a4c231d4ae53
David Moulton <d.moulton@elifesciences.org>
2016-12-13 00:33:30 +0000

    Improve carousel navigation

--- assets/js/components/Carousel.js
+++ assets/js/components/Carousel.js
@@ -14,16 +14,23 @@ module.exports = class Carousel {
     // 1-indexed not 0-indexed as will be used as a multiplier
     this.currentSlide = 1;
     this.slideCount = this.$elm.querySelectorAll('.carousel__item').length;
+    if (this.slideCount === 1) {
+      return;
+    }
+
     this.moveableStage = this.$elm.querySelector('.carousel__items');
 
     // Set width based on number of items available.
     this.moveableStage.style.width = (this.slideCount * 100) + 'vw';
-    this.$elm.querySelector('.carousel__control--previous').addEventListener(
-                                                                          'click',
-                                                                          this.previous.bind(this));
-    this.$elm.querySelector('.carousel__control--next').addEventListener(
-                                                                          'click',
-                                                                          this.next.bind(this));
+    this.switches = this.$elm.querySelectorAll('.carousel__control--switch');
+    this.buttons = {
+      previous: this.$elm.querySelector('.carousel__control--previous'),
+      next: this.$elm.querySelector('.carousel__control--next')
+    };
+    this.buttons.previous.addEventListener('click', this.previous.bind(this));
+    this.buttons.next.addEventListener('click', this.next.bind(this));
+    this.$elm.querySelector('.carousel__control_switches').addEventListener('click',
+                                                                   this.activateSwitch.bind(this));
 
     // TODO: move controls from mustache to js.
   }
@@ -31,15 +38,16 @@ module.exports = class Carousel {
   next() {
     if (this.currentSlide < this.slideCount) {
       this.updateSlide();
-      this.currentSlide += 1;
+      this.updateControlPanel(this.currentSlide);
     }
   }
 
   previous() {
     if (this.currentSlide > 1) {
       this.updateSlide('previous');
-      this.currentSlide -= 1;
+      this.updateControlPanel(this.currentSlide);
     }
+
   }
 
   getCurrentOffset() {
@@ -53,10 +61,63 @@ module.exports = class Carousel {
 
   updateSlide(direction) {
     let currentOffset = this.getCurrentOffset();
-    let newOffset = direction === 'previous' ? currentOffset + 100 : currentOffset - 100;
+    let newOffset;
+
+    if (direction === 'previous') {
+      this.currentSlide -= 1;
+      newOffset = currentOffset + 100;
+    } else {
+      this.currentSlide += 1;
+      newOffset = currentOffset - 100;
+    }
 
     // TODO: Consider updating utils with this forumlation (vw) for translation adjustment
     this.moveableStage.style.transform = 'translateX(' + newOffset + 'vw)';
+  }
+
+  hideInvalidButtonChoice(currentSlide) {
+    if (currentSlide === this.slideCount) {
+      this.buttons.next.classList.add('hidden');
+      this.buttons.previous.classList.remove('hidden');
+    }
+
+    if (currentSlide === 1) {
+      this.buttons.previous.classList.add('hidden');
+      this.buttons.next.classList.remove('hidden');
+    }
+  }
+
+  updateActiveSwitch(currentSlide) {
+    [].forEach.call(this.switches, (aSwitch, i) => {
+      if (i === currentSlide - 1) {
+        aSwitch.classList.add('active');
+      } else {
+        aSwitch.classList.remove('active');
+      }
+    });
+  }
+
+  updateControlPanel(currentSlide) {
+    // this.hideInvalidButtonChoice(currentSlide);
+    this.updateActiveSwitch(currentSlide);
+  }
+
+  activateSwitch(e) {
+    let activatedSwitch = e.target;
+    let activatedSlide = this.window.parseInt(activatedSwitch.querySelector('span').innerHTML, 10);
+    if (activatedSlide === this.currentSlide) {
+      return;
+    }
+
+    let slideOffSet = activatedSlide - this.currentSlide;
+    let slideOffSetAbs = Math.abs(slideOffSet);
+    let callback = slideOffSet < 0 ? this.previous : this.next;
+
+    for (let i = 0; i < slideOffSetAbs; i += 1) {
+      // this[callback].bind(this);
+      callback.call(this);
+    }
+
   }
 
 };
