06beb6d0aff631964591251320d88f175109d2d6
David Moulton <d.moulton@elifesciences.org>
2016-12-16 15:55:04 +0000

    Refactor, add docs, fix resize bug

--- assets/js/components/Carousel.js
+++ assets/js/components/Carousel.js
@@ -15,9 +15,10 @@ module.exports = class Carousel {
 
     this.moveableStage = this.$elm.querySelector('.carousel__items');
     this.originalSlideWrappers = this.moveableStage.querySelectorAll('.carousel__item_wrapper');
-    this.slideCount = this.originalSlideWrappers.length;
+    this.originalSlideCount = this.originalSlideWrappers.length;
+    this.currentSlideCount = this.originalSlideCount;
 
-    if (this.slideCount < 2) {
+    if (this.originalSlideCount < 2) {
       return;
     }
 
@@ -26,46 +27,64 @@ module.exports = class Carousel {
   }
 
   init() {
-    // 1-indexed not 0-indexed as will be used as a multiplier
     this.currentSlide = 1;
-    this.timerInterval = 3000;
+    this.timerInterval = 10000;
 
     this.$elm.appendChild(this.buildControls());
     this.updateWidth();
     this.updateControlPanel(this.currentSlide);
-    this.togglePlay();
+    this.togglePlay(this.toggler.querySelector('button'));
 
+    this.window.addEventListener('keydown', this.handleKey.bind(this));
     this.window.addEventListener('blur', this.cancelTimer.bind(this));
     this.window.addEventListener('focus', this.setupTimer.bind(this));
-    this.window.addEventListener('resize', this.updateWidth.bind(this));
+    this.window.addEventListener('resize', () => {
+      this.updateWidth();
+      this.adjustTranslateForResize();
+    });
   }
 
+  /**
+   * Builds and returns all controls for the carousel
+   *
+   * @returns {Element} The control panel
+   */
   buildControls() {
     // Btn wrappers needed to reserve space in case buttons are removed, to stop things moving about
     let $previousWrapper = this.buildControl$traverser('previous');
     let $nextWrapper = this.buildControl$traverser('next');
 
     this.buttons = {
-      playToggle: this.buildControl$toggle(),
       previous: $previousWrapper.querySelector('button'),
       next: $nextWrapper.querySelector('button')
     };
-    this.switches = this.buildControl$switches(this.slideCount);
 
+    // switches are the circular buttons for depicting/going to a slide
+    this.switches = this.buildControl$switches(this.originalSlideCount);
+
+    this.toggler = this.buildControl$toggle();
+
+    let $controlPanel = utils.buildElement('div', ['carousel__control_panel']);
+    $controlPanel.appendChild(this.toggler);
+
+    // Separate from the toggler, as that's visually hidden
     let $visibleControlsWrapper = utils.buildElement('div', ['carousel__control_panel__visible']);
     let visibleControls = [$previousWrapper, this.switches, $nextWrapper];
     visibleControls.forEach(function (control) {
       $visibleControlsWrapper.appendChild(control);
     });
 
-    let $controlPanel = utils.buildElement('div', ['carousel__control_panel']);
-    $controlPanel.appendChild(this.buttons.playToggle);
     $controlPanel.appendChild($visibleControlsWrapper);
 
-    this.setupEventListeners();
     return $controlPanel;
   }
 
+  /**
+   * Returns a div-wrapped button for a next or previous control, with event listener
+   *
+   * @param {String} direction 'previous' | 'next'
+   * @returns {Element} A div-wrapped button
+   */
   buildControl$traverser(direction) {
     let _direction = direction === 'previous' ? 'previous' : 'next';
     let text = direction + ' item';
@@ -77,11 +96,19 @@ module.exports = class Carousel {
                                      ],
                                      '',
                                      $wrapper);
+    $button.addEventListener('click', () => {
+      this.userInitiatedProgression(this[_direction]);
+    });
 
     utils.buildElement('span', ['visuallyhidden'], text, $button);
     return $wrapper;
   }
 
+  /**
+   * Builds and returns a visually hidden play/pause toggle (button & label), with event listener
+   *
+   * @returns {Element} The toggler
+   */
   buildControl$toggle() {
     let buttonId = 'carouselToggle';
     let $button = utils.buildElement('button', ['carousel__control--toggler'], 'Play');
@@ -95,18 +122,34 @@ module.exports = class Carousel {
     let $toggle = utils.buildElement('div', ['carousel__control_toggle', 'visuallyhidden']);
     $toggle.appendChild($label);
     $toggle.appendChild($button);
+    $toggle.addEventListener('click', () => {
+      this.togglePlay($button);
+    });
+
     return $toggle;
   }
 
+  /**
+   * Builds and returns a list of switches, with event listener attached
+   *
+   * @param quantity The number of switches to build
+   * @returns {Element} The list of switches
+   */
   buildControl$switches(quantity) {
-    let $list = utils.buildElement('ol', ['carousel__control_switches']);
+    let $switches = utils.buildElement('ol', ['carousel__control_switches']);
     for (let i = 1; i <= quantity; i += 1) {
-      $list.appendChild(this.buildControl$switch(i));
+      $switches.appendChild(this.buildControl$switch(i));
     }
 
-    return $list;
+    $switches.addEventListener('click', this.activateSwitch.bind(this));
+    return $switches;
   }
 
+  /**
+   * Builds and returns a switch
+   * @param text The next for the switch (visually hidden)
+   * @returns {Element} The switch
+   */
   buildControl$switch(text) {
     let $item = utils.buildElement('li', ['carousel__control--switch-item']);
     let $button =  utils.buildElement('button', ['carousel__control--switch'], '', $item);
@@ -114,32 +157,19 @@ module.exports = class Carousel {
     return $item;
   }
 
-  setupEventListeners() {
-    this.buttons.playToggle.addEventListener('click', this.togglePlay.bind(this));
-
-    this.window.addEventListener('keydown', this.handleKey.bind(this));
-
-    this.buttons.previous.addEventListener('click', () => {
-      this.userInitiatedProgression(this.previous);
-    });
-
-    this.switches.addEventListener('click', this.activateSwitch.bind(this));
-
-    this.buttons.next.addEventListener('click', () => {
-      this.userInitiatedProgression(this.next);
-    });
-
-  }
-
-  togglePlay() {
-    let toggleButton = this.buttons.playToggle.querySelector('button');
-    let currentButtonState = toggleButton.innerHTML;
+  /**
+   * Toggles the play/pause state of the carousel based on the current value of the toggle button.
+   *
+   * @param $button The button to use as the toggle
+   */
+  togglePlay($button) {
+    let currentButtonState = $button.innerHTML;
     if (currentButtonState === 'Pause') {
-      toggleButton.innerHTML = 'Play';
+      $button.innerHTML = 'Play';
       this.timerStopped = true;
       this.cancelTimer();
     } else {
-      toggleButton.innerHTML = 'Pause';
+      $button.innerHTML = 'Pause';
       this.timerStopped = false;
       this.setupTimer();
     }
@@ -179,7 +209,7 @@ module.exports = class Carousel {
 
   handleKey(e) {
     let code = e.keyCode || e.charCode;
-    if (code === 37 || code === 39) {
+    if (code === 37/*left arrow*/ || code === 39/*right arrow*/) {
       this.timerStopped = true;
       if (code === 37) {
         this.userInitiatedProgression(this.previous);
@@ -190,6 +220,7 @@ module.exports = class Carousel {
   }
 
   userInitiatedProgression(callback) {
+    // Any user-activation of a control permanently stops the timer
     this.timerStopped = true;
     if (!!callback && typeof callback === 'function') {
       callback.call(this, true);
@@ -197,7 +228,7 @@ module.exports = class Carousel {
   }
 
   next() {
-    if (this.currentSlide % this.slideCount === 0) {
+    if (this.currentSlide % this.originalSlideCount === 0) {
       this.extendStage();
     }
 
@@ -213,18 +244,26 @@ module.exports = class Carousel {
   }
 
   extendStage () {
-    this.updateWidth(true);
+    // Make room for another slide set
+    this.currentSlideCount += this.originalSlideCount;
+    this.updateWidth();
+
+    // Clone in the new slide set
     [].forEach.call(this.originalSlideWrappers, (slide) => {
       this.moveableStage.appendChild(slide.cloneNode(true));
     });
   }
 
+  /**
+   * Translates moveableStage 1 slide's width in the appropriate direction
+   * @param {String} direction 'next' | 'previous' Direction to move
+   */
   updateSlide(direction) {
     let currentOffset = this.getCurrentOffset();
-    let newOffset;
     let currentWidthStringMatch = this.window.getComputedStyle(this.$elm).width
                                       .match(/^([0-9]+)px/);
     let currentCarouselWidth = this.window.parseInt(currentWidthStringMatch[1], 10);
+    let newOffset;
     if (direction === 'previous') {
       this.currentSlide -= 1;
       newOffset = currentOffset + currentCarouselWidth;
@@ -236,6 +275,11 @@ module.exports = class Carousel {
     utils.updateElementTranslate(this.moveableStage, [newOffset + 'px', 0]);
   }
 
+  /**
+   * Returns the current numerical value of moveableStage's X translation in px
+   *
+   * @returns {number} Numerical value of the X translation of moveableStage
+   */
   getCurrentOffset() {
     let offsetStringMatch = this.moveableStage.style.transform.match(/translate\((.*)px,[^)]+\)/);
     if (!offsetStringMatch) {
@@ -250,6 +294,11 @@ module.exports = class Carousel {
     this.updateActiveSwitch(currentSlide);
   }
 
+  /**
+   * Hides previous button if on first slide.
+   *
+   * @param {number} currentSlide The number of the current slide
+   */
   hideInvalidButtonChoice(currentSlide) {
     if (currentSlide === 1) {
       this.buttons.previous.classList.add('hidden');
@@ -258,6 +307,11 @@ module.exports = class Carousel {
     }
   }
 
+  /**
+   * Activate the switch indicating the current logical slide
+   *
+   * @param {number} currentSlide The number of the current slide
+   */
   updateActiveSwitch(currentSlide) {
     let currentLogicalSlide = this.getLogicalSlideNumber(currentSlide);
     [].forEach.call(this.switches.querySelectorAll('.carousel__control--switch'), (aSwitch, i) => {
@@ -269,13 +323,28 @@ module.exports = class Carousel {
     });
   }
 
+  /**
+   * Returns the number of the side within its set.
+   *
+   * E.g. where there's 3 slides in a set, and the actualSlideNumber is 8, this indicates that it's
+   * the 2nd slide in the 3rd slide set, so its logical number is 2.
+   * (A slide set is a clone of the starting slides to facilitate perpetual scroll.)
+   *
+   * @param {number} actualSlideNumber Number of the slide to evaluate
+   * @returns {number}
+   */
   getLogicalSlideNumber(actualSlideNumber) {
-    let setSize = this.originalSlideWrappers.length;
+    let setSize = this.originalSlideCount;
     let slideSetOfSlide = Math.ceil(actualSlideNumber / setSize);
     let firstSlideOfSet = setSize *  slideSetOfSlide - (setSize - 1);
     return actualSlideNumber - firstSlideOfSet + 1;
   }
 
+  /**
+   * Move to the closest slide with the same logical number as the activated switch
+   *
+   * @param {Event} e The event
+   */
   activateSwitch(e) {
     let logicalSlideRequested = this.window.parseInt(e.target.querySelector('span').innerHTML, 10);
     let logicalSlideCurrent = this.getLogicalSlideNumber(this.currentSlide);
@@ -292,17 +361,18 @@ module.exports = class Carousel {
     }
   }
 
-  updateWidth(stageExtended) {
-    if (!stageExtended) {
-      this.moveableStage.style.width = (this.$elm.width * this.slideCount) + 'px';
-      return;
-    }
+  adjustTranslateForResize() {
+    let carouselWidth = window.getComputedStyle(this.$elm).width.match(/([0-9]+)px/)[1];
+    let currentSlide = this.currentSlide;
+    let expectedOffset = (currentSlide - 1) * carouselWidth * -1;
+    this.moveableStage.style.transform = 'translate(' + expectedOffset + 'px, 0)';
+  }
 
-    // increment width to accommodate more slides
-    let currentWidthString = this.window.getComputedStyle(this.$elm).width;
-    let currentWidth = currentWidthString.match(/([0-9]+)px/)[1];
-    let increment = currentWidth * this.slideCount;
-    this.moveableStage.style.width = currentWidth + increment + 'px';
+  updateWidth() {
+    this.moveableStage.style.width = utils.adjustPxString(
+      this.window.getComputedStyle(this.$elm).width,
+      this.currentSlideCount,
+      'multiply');
   }
 
 };
