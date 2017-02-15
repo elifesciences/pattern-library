8860024353f0ff049b205eb2c567029db0f71479
Scott Gemmell <scott.gemmell@mac.com>
2017-02-15 10:29:04 +0000

    Feature/elpp 2073 (#504)
    * Renamed all-subjects-list component to section-listing
    
    * Created About Elife Page to showcase Section Listing
    
    * Renamed AllSubjectsListLink javascript to SectionListingLink
    
    * Updated home template to reflect beta and updated About template to show single line Section Listing
    
    * Updated SectionListing.js to resize for Mobile and Desktop breakpoints
    
    * Linting and refactoring
    
    * Dev command (#501)
    
    * Added bin/dev command
    
    * Added docs and port param
    
    * Rearrange JS components in alphabetical order
    
    * Delete empty media query in scss
    
    * Removed duplicated a11y list ID
    
    * Removed unused function
    
    * Renamed subject variables to section
    
    * Renamed  to
    
    * classList not className
    
    * Utilising new debounce utility snippet
    
    * Updated YAML, refactore show/hide Triggers
    
    * Renaming variables
    
    * linting fixes
    
    * subjects changed to sections in Section Listing json modifier
    
    * Added $ to HTML element variables
    
    * class names change, return in alphabetical order
    
    * Debugging
    
    * Temporarily hide comments
    
    * Reduced unnecessary resize calls
    
    * Removed comment

---
+++ assets/js/components/SectionListingLink.js
@@ -0,0 +1,88 @@
+'use strict';
+var utils = require('../libs/elife-utils')();
+
+module.exports = class SectionListingLink {
+
+  constructor($elm, _window = window, doc = document) {
+
+    if (!($elm instanceof HTMLElement)) {
+      return;
+    }
+
+    this.window = _window;
+    this.thresholdWidth = 740;
+    this.$elm = $elm;
+    this.doc = doc;
+    this.$list = this.doc.querySelector(SectionListingLink.findIdSelector($elm.href));
+    this.$theListParent = this.$list.parentNode;
+    this.$theElmParent = $elm.parentNode;
+    this.$theListing = this.doc.getElementById('sectionsListing');
+    this.displayBreakpoint();
+
+    this.isMobile = null;
+
+    this.window.addEventListener('resize',
+      utils.debounce(() => this.displayBreakpoint(), 100)
+    );
+
+  }
+
+  hideElm() {
+
+    this.$elm.classList.add('visuallyhidden');
+
+  }
+
+  showElm() {
+
+    this.$elm.classList.remove('visuallyhidden');
+
+  }
+
+  displayBreakpoint() {
+
+    if (this.$list && this.viewportNoWiderThan(this.thresholdWidth)) {
+
+      this.switchToMobilePosition();
+      this.showElm();
+
+    } else {
+
+      this.switchToDesktopPosition();
+      this.hideElm();
+    }
+  }
+
+  switchToMobilePosition() {
+
+    if (this.isMobile === true) {
+      return;
+    }
+
+    this.isMobile = true;
+    this.$theListParent.appendChild(this.$theListing);
+
+  }
+
+  switchToDesktopPosition() {
+
+    if (this.isMobile === false) {
+      return;
+    }
+
+    this.isMobile = false;
+    this.$theElmParent.appendChild(this.$theListing);
+
+  }
+
+  viewportNoWiderThan(thresholdInPx) {
+    return this.window.matchMedia('(max-width: ' + thresholdInPx + 'px)').matches;
+  }
+
+  static findIdSelector(href) {
+    if (href) {
+      return href.substring(href.indexOf('#'));
+    }
+  }
+
+};
