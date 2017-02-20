c31f0beeedfa9cde33b51a0134adb97be0ddaa06
Scott Gemmell <scott.gemmell@mac.com>
2017-02-20 16:08:23 +0000

    Prev/Next working on refresh

--- assets/js/components/Highlighted.js
+++ assets/js/components/Highlighted.js
@@ -16,6 +16,8 @@ module.exports = class Highlighted {
     this.window = _window;
     this.$elm = $elm;
     this.doc = doc;
+    this.tabletWidth = 640;
+    this.desktopWidth = 980;
 
     const prevBtn = doc.querySelector('#trigger-prev');
     const nextBtn = doc.querySelector('#trigger-next');
@@ -28,9 +30,9 @@ module.exports = class Highlighted {
     //this.originalSlideCount = this.originalSlideWrappers.length;
     //this.currentSlideCount = this.originalSlideCount;
 
-    console.log("$elm: ", this.$elm);
-    console.log("moveableStage: ", this.moveableStage);
-    console.log("originalSlideWrappers: ", this.originalSlideWrappers);
+    //console.log("$elm: ", this.$elm);
+    //console.log("moveableStage: ", this.moveableStage);
+    //console.log("originalSlideWrappers: ", this.originalSlideWrappers);
     //console.log("originalSlideCount: ", this.originalSlideCount);
     //console.log("currentSlideCount: ", this.currentSlideCount);
 
@@ -42,24 +44,34 @@ module.exports = class Highlighted {
     //   this.adjustTranslateForResize();
     // });
 
+    this.switchBreakpoint();
+    this.window.addEventListener('resize',
+      utils.debounce(() => this.switchBreakpoint(), 100)
+    );
+
+
+    var total = this.totalSlides;
+
+
     prevBtn.addEventListener('click', () => {
 
       this.currentSlide == 1 ? this.currentSlide = 1 : this.currentSlide -= 1;
 
       console.log("prevBtn click", this.currentSlide);
 
-      this.downOne();
+      this.slidePrev();
       this.adjustTranslateForResize();
 
     });
 
+
     nextBtn.addEventListener('click', () => {
 
-      this.currentSlide == 5 ? this.currentSlide = 5 : this.currentSlide += 1;
+      this.currentSlide == total ? this.currentSlide = total : this.currentSlide += 1;
 
       console.log("nextBtn click", this.currentSlide);
 
-      this.upOne();
+      this.slideNext();
       this.adjustTranslateForResize();
 
     });
@@ -72,12 +84,67 @@ module.exports = class Highlighted {
 
   }
 
-  upOne(){
-    console.log("upOne");
+  viewportNoWiderThan(thresholdInPx) {
+    return this.window.matchMedia('(max-width: ' + thresholdInPx + 'px)').matches;
   }
 
-  downOne(){
-    console.log("downOne");
+  resetStage(){
+    //console.log("resetStage()");
+    this.moveableStage.style.transform = 'translate(0px, 0)';
+  }
+
+  switchBreakpoint() {
+
+    this.resetStage();
+
+    if (this.viewportNoWiderThan(this.tabletWidth)) {
+
+      this.viewport = "MOBILE";
+      this.inView = 1;
+      this.totalSlides = 5;
+
+      this.switchToMobilePosition();
+
+    } else if (this.viewportNoWiderThan(this.desktopWidth)) {
+
+      this.viewport = "TABLET";
+      this.inView = 2;
+      this.totalSlides = 4;
+
+      this.switchToTabletPosition();
+
+    } else {
+
+      this.viewport = "DESKTOP";
+      this.inView = 3;
+      this.totalSlides = 3;
+
+      this.switchToDesktopPosition();
+    }
+
+    console.log("viewport: ", this.viewport);
+    console.log("inView: ", this.inView);
+    console.log("totalSlides: ", this.totalSlides);
+  }
+
+  switchToMobilePosition() {
+    //console.log("MOBILE");
+  }
+
+  switchToTabletPosition() {
+    //console.log("TABLET");
+  }
+
+  switchToDesktopPosition() {
+    //console.log("DESKTOP");
+  }
+
+  slideNext(){
+    console.log("slideNext");
+  }
+
+  slidePrev(){
+    console.log("slidePrev");
   }
 
   init() {
@@ -98,7 +165,7 @@ module.exports = class Highlighted {
     let carouselWidth = window.getComputedStyle(this.$elm).width.match(/([0-9\.]+)px/)[1];
     let currentSlide = this.currentSlide;
     let expectedOffset = (currentSlide - 1) * carouselWidth * -1;
-    this.moveableStage.style.transform = 'translate(' + expectedOffset + 'px, 0)';
+    this.moveableStage.style.transform = 'translate(' + (expectedOffset / this.inView) + 'px, 0)';
   }
 
 
