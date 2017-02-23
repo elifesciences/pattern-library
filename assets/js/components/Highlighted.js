60fba401f21f4ae72470a8c870a6b186307e9c26
Scott Gemmell <scott.gemmell@mac.com>
2017-02-23 11:56:32 +0000

    Slide limits working for all breakpoints

--- assets/js/components/Highlighted.js
+++ assets/js/components/Highlighted.js
@@ -1,7 +1,7 @@
 'use strict';
 var utils = require('../libs/elife-utils')();
 
-console.log("Highlighted.js - 1");
+console.log("Highlighted.js - a");
 
 module.exports = class Highlighted {
 
@@ -38,7 +38,6 @@ module.exports = class Highlighted {
     //console.log("originalSlideCount: ", this.originalSlideCount);
     //console.log("currentSlideCount: ", this.currentSlideCount);
 
-
     //this.adjustTranslateForResize();
 
     // this.window.addEventListener('resize', () => {
@@ -51,12 +50,11 @@ module.exports = class Highlighted {
       utils.debounce(() => this.switchBreakpoint(), 100)
     );
 
-    let total = this.totalSlides;
-
 
     prevBtn.addEventListener('click', () => {
 
-      this.currentSlide == 1 ? this.currentSlide = 1 : this.currentSlide -= 1;
+      this.currentSlide === 1 ? this.currentSlide = 1 : this.currentSlide -= 1;
+      //this.updateButtons();
 
       console.log("prevBtn click", this.currentSlide);
 
@@ -68,7 +66,8 @@ module.exports = class Highlighted {
 
     nextBtn.addEventListener('click', () => {
 
-      this.currentSlide == total ? this.currentSlide = total : this.currentSlide += 1;
+      this.currentSlide === this.maxSlide ? this.currentSlide = this.maxSlide : this.currentSlide += 1;
+      //this.updateButtons();
 
       console.log("nextBtn click", this.currentSlide);
 
@@ -86,6 +85,7 @@ module.exports = class Highlighted {
   resetStage(){
     //console.log("resetStage()");
     this.moveableStage.style.transform = 'translate(0px, 0)';
+    this.currentSlide = 1;
   }
 
   switchBreakpoint() {
@@ -96,7 +96,7 @@ module.exports = class Highlighted {
 
       this.viewport = "MOBILE";
       this.inView = 1;
-      this.totalSlides = 5;
+      this.maxSlide = 5;
 
       this.switchToMobilePosition();
 
@@ -104,7 +104,7 @@ module.exports = class Highlighted {
 
       this.viewport = "TABLET";
       this.inView = 2;
-      this.totalSlides = 4;
+      this.maxSlide = 4;
 
       this.switchToTabletPosition();
 
@@ -112,7 +112,7 @@ module.exports = class Highlighted {
 
       this.viewport = "DESKTOP";
       this.inView = 3;
-      this.totalSlides = 3;
+      this.maxSlide = 3;
 
       this.switchToDesktopPosition();
     }
@@ -133,24 +133,72 @@ module.exports = class Highlighted {
   }
 
   slideNext(){
-    console.log("slideNext");
+    //console.log("slideNext");
   }
 
   slidePrev(){
-    console.log("slidePrev");
+    //console.log("slidePrev");
   }
 
   adjustTranslateForResize() {
+
+    console.log("currentSlide: ", this.currentSlide);
 
     console.log("Highlighted.js - adjustTranslateForResize()");
 
     let carouselWidth = window.getComputedStyle(this.$elm).width.match(/([0-9\.]+)px/)[1];
     let currentSlide = this.currentSlide;
     let expectedOffset = (currentSlide - 1) * carouselWidth * -1;
-    this.moveableStage.style.transform = 'translate(' + (expectedOffset / this.inView) + 'px, 0)';
+    //this.moveableStage.style.transform = 'translate(' + (expectedOffset / this.inView) + 'px, 0)';
+
+    console.log("carouselWidth: " + carouselWidth + ", currentSlide: " + currentSlide + ", expectedOffset: " + expectedOffset + ", this.inView: " + this.inView);
+    //MOBILE: let increment = expectedOffset / this.inView;
+    //TABLET: let increment = (expectedOffset / this.inView) - ((currentSlide - 1) * 20);
+    // DESKTOP: let increment = ((expectedOffset + 80 )/ this.inView) - ((currentSlide - 1) * 40);
+    let increment = expectedOffset / this.inView;
+    console.log("increment: ", increment);
+
+    this.moveableStage.style.transform = 'translate(' + increment + 'px, 0)';
+
+
   }
 
 
+  updateButtons(){
+    //console.log("updateButtons");
+
+
+    if(this.currentSlide == 1) {
+
+      console.log("currentSlide :first ", this.currentSlide);
+      //this.hideBtn();
+
+
+    } else if(this.currentSlide == this.region) {
+
+      console.log("currentSlide :last ", this.currentSlide);
+      //this.hideBtn();
+
+
+    } else {
+
+      console.log("currentSlide", this.currentSlide);
+
+    }
+
+  }
+
+  hideBtn(e){
+
+    //console.log("hideBtn", e);
+
+  }
+
+  showBtn(e){
+
+    //console.log("showBtn", e);
+
+  }
 
   // updateWidth() {
   //
