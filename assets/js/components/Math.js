6387aac5b3ff0476b91035b4cf411d347e6693b9
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-04-10 08:57:34 +0100

    Update MathJax CDN

--- assets/js/components/Math.js
+++ assets/js/components/Math.js
@@ -52,7 +52,9 @@ module.exports = class Math {
     let script = doc.createElement('script');
     script.type = 'text/javascript';
     script.addEventListener('load', Math.setupResizeHandler);
-    script.src  = 'https://cdn.mathjax.org/mathjax/2.7-latest/MathJax.js?config=MML_HTMLorMML';
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=MML_HTMLorMML';
+    script.integrity = 'sha384-crwIf/BuaWM9rM65iM+dWFldgQ1Un8jWZMuh3puxb8TOY9+linwLoI7ZHZT+aekW';
+    script.crossOrigin = 'anonymous';
     doc.querySelector('body').appendChild(script);
   }
 
