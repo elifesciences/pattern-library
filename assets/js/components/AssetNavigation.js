a2a064c7070a3a7898f8cb367d0027848400eeb2
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-05-08 14:01:51 +0100

    forEach()

--- assets/js/components/AssetNavigation.js
+++ assets/js/components/AssetNavigation.js
@@ -10,16 +10,13 @@ module.exports = class AssetNavigation {
     this.assetItems = [this.$elm];
 
     const supplements = this.findSupplements();
-    for (let i = 0; i < supplements.length; i += 1) {
-      this.assetItems.push(supplements[i]);
-    }
+    [].forEach.call(supplements, (supplement) => this.assetItems.push(supplement));
 
     if (this.assetItems.length > 1) {
       const hash = this.window.location.hash.substring(1);
       let show = 0;
 
-      for (let i = 0; i < this.assetItems.length; i++) {
-        const assetItem = this.assetItems[i];
+      [].forEach.call(this.assetItems, (assetItem, i) => {
         const navigation = utils.buildElement('div', ['asset-viewer-inline__header_navigation'], '', assetItem.querySelector('.asset-viewer-inline__header_panel'), '.asset-viewer-inline__header_text');
 
         if (assetItem.id === hash) {
@@ -28,7 +25,7 @@ module.exports = class AssetNavigation {
 
         this.addPreviousButton(i, navigation);
         this.addNextButton(i, navigation);
-      }
+      });
 
       this.show(show);
 
@@ -57,9 +54,7 @@ module.exports = class AssetNavigation {
   }
 
   hideAll() {
-    for (let i = 0; i < this.assetItems.length; i++) {
-      this.assetItems[i].classList.add('visuallyhidden');
-    }
+    [].forEach.call(this.assetItems, (assetItem) => assetItem.classList.add('visuallyhidden'));
   }
 
   show(i) {
@@ -85,11 +80,11 @@ module.exports = class AssetNavigation {
       return false;
     }
 
-    for (let i = 0; i < this.assetItems.length; i++) {
+    [].forEach.call(supplements, () => function (assetItem, i) {
       if (this.assetItems[i].id === hash) {
         this.show(i);
       }
-    }
+    });
   }
 
 };
