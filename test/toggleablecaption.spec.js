0d7877affe7d979786c7f7823d2bd22609f9711d
David Moulton <davidcmoulton@users.noreply.github.com>
2017-05-26 11:16:57 +0100

    Feature/collapsible asset captions (#652)
    * Begin caption toggle
    
    * Get unstyled toggle working
    
    * Update toggle style & add changing state
    
    * Refine toggle appearance
    
    * Add comment
    
    * Use library
    
    * If no MathJax at all
    
    * Make caption toggling part of the AssetViewerInline
    
    * Reset
    
    * Add basic tests

---
+++ test/toggleablecaption.spec.js
@@ -0,0 +1,29 @@
+let expect = chai.expect;
+
+// Babel polyfill
+require('../node_modules/babel-polyfill');
+
+// load in component(s) to be tested
+let ToggleableCaption = require('../assets/js/components/ToggleableCaption');
+
+describe('A ToggleableCaption Component', function () {
+  'use strict';
+  let $elm = document.querySelector('[data-behaviour="ToggleableCaption"]');
+  let toggleableCaption = new ToggleableCaption($elm);
+
+  it('exists', function () {
+    expect(toggleableCaption).to.exist;
+  });
+
+  it('truncates the caption', function () {
+    expect(toggleableCaption.$caption.querySelector('.caption-text__toggle--see-more')).to.exist;
+    expect(toggleableCaption.$caption.querySelector('.caption-text__toggle--see-less')).to.not.exist;
+  });
+
+  it('collapses the caption', function () {
+    toggleableCaption.toggleCaption();
+    expect(toggleableCaption.$caption.querySelector('.caption-text__toggle--see-more')).to.not.exist;
+    expect(toggleableCaption.$caption.querySelector('.caption-text__toggle--see-less')).to.exist;
+  });
+
+});
