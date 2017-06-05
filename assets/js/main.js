62c57bf951a13ccac169c469872fb57e6f6a1022
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-06-05 08:10:21 +0100

    Only use MutationObserver if it exists

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,13 +84,15 @@ if (window.localStorage && document.querySelector &&
       [].forEach.call(components, (el) => initialiseComponent(el));
     }
 
-    let observer = new MutationObserver(() => {
-      let components = document.querySelectorAll('[data-behaviour]:not([data-behaviour-initialised])');
-      if (components) {
-        [].forEach.call(components, (el) => initialiseComponent(el));
-      }
-    });
-    observer.observe(document.body, { childList: true, subtree: true });
+    if ('MutationObserver' in window) {
+      let observer = new MutationObserver(() => {
+        let components = document.querySelectorAll('[data-behaviour]:not([data-behaviour-initialised])');
+        if (components) {
+          [].forEach.call(components, (el) => initialiseComponent(el));
+        }
+      });
+      observer.observe(document.body, { childList: true, subtree: true });
+    }
   };
 
   new Elife();
