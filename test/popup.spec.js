f3f41e1d97358baa4adda1ee438f5dffcb614214
Giorgio Sironi <g.sironi@elifesciences.org>
2017-07-12 17:31:23 +0100

    Adding data-popup-wrapper configuration

--- test/popup.spec.js
+++ test/popup.spec.js
@@ -22,4 +22,13 @@ describe('A Popup Component', function () {
     expect(popup.isOpen).to.be.false;
   });
 
+  it('wraps and popups self', function () {
+    let $childElm = document.querySelector('#self-example-wrapped[data-behaviour="Popup"]');
+    let popup = new Popup($childElm);
+    popup.$link.click();
+    expect(popup.isOpen).to.be.true;
+    document.querySelector('body').click();
+    expect(popup.isOpen).to.be.false;
+  });
+
 });
