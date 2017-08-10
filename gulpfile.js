faab57afc766a677dc87e7ccb3e23a921f96ae48
David Moulton <d.moulton@elifesciences.org>
2017-08-10 17:42:41 +0100

    Add tests for SectionListingLink component

--- gulpfile.js
+++ gulpfile.js
@@ -282,7 +282,7 @@ gulp.task('server', () => {
     server = express();
     server.use(express.static('./'));
     server.listen('8080');
-    browserSync({proxy: 'localhost:8080', startPath: 'test/viewselector.html'});
+    browserSync({proxy: 'localhost:8080', startPath: 'test/sectionlistinglink.html'});
   } else {
     return gutil.noop;
   }
