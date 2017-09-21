57549d547fd483366eda393e00969885f8d82ac6
Giorgio Sironi <g.sironi@elifesciences.org>
2017-09-21 13:25:42 +0000

    Hello world test
    Run with

---
+++ test-selenium/hello.spec.js
@@ -0,0 +1,10 @@
+let chai = require('chai');
+let expect = chai.expect;
+
+describe('webdriver.io page', function() {
+  it('should have the right title', function () {
+    browser.url('http://localhost');
+    var title = browser.getTitle();
+    expect(title).to.equal('Pattern Lab');
+  });
+});
