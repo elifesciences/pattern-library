4f2e1de98828b7fe8aa3a16ca303f1f4e3735ac6
Giorgio Sironi <g.sironi@elifesciences.org>
2017-09-25 15:06:36 +0100

    Sticking to the default reporter for now

--- wdio-local.conf.js
+++ wdio-local.conf.js
@@ -119,7 +119,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/testrunner/reporters.html
-    reporters: ['spec'],
+    reporters: ['dot'],
     
     //
     // Options to be passed to Mocha.
