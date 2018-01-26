9ce52e5fc7b7181976317cab0dde4269c68f5dc8
David Moulton <d.moulton@elifesciences.org>
2018-01-26 10:52:51 +0000

    Delay opening sections via hash to avoid occasional failure

--- assets/js/components/FragmentHandler.js
+++ assets/js/components/FragmentHandler.js
@@ -15,20 +15,8 @@ module.exports = class FragmentHandler {
     this.window = _window;
     this.doc = doc;
     this.isSingleton = true;
-    this.window.addEventListener('readystatechange', this.onReadyStateChange.bind(this));
-    this.onReadyStateChange({});
-  }
 
-  onReadyStateChange(e) {
-    if (document.readyState !== 'loading') {
-      this.initialise(e);
-      this.window.removeEventListener('readystatechange', this.onReadyStateChange.bind(this));
-    }
-  }
-
-  initialise(e) {
-    this.handleSectionOpeningViaHash(e);
-    this.window.addEventListener('hashchange', this.handleSectionOpeningViaHash.bind(this));
+    this.window.addEventListener('load', this.handleSectionOpeningViaHash.bind(this));
   }
 
   /**
