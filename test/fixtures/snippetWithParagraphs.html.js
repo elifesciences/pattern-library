5ef2046206a17b91303e8f6399711121b38d7b4c
David Moulton <d.moulton@elifesciences.org>
2018-02-20 09:52:40 +0000

    Improve supplying paragraphs from test fixture

--- test/fixtures/snippetWithParagraphs.html.js
+++ test/fixtures/snippetWithParagraphs.html.js
@@ -1,18 +1,20 @@
 const utils = require('../../assets/js/libs/elife-utils')();
 const buildElement = utils.buildElement;
 
-module.exports = (count) => {
+module.exports = (requestedCount) => {
   'use strict';
 
   const $root = buildElement('div');
   const $articleSection = buildElement('div', ['article-section'], '', $root);
   const $articleSectionBody = buildElement('div', ['article-section__body'], '', $articleSection);
-  if (!count) {
-    return $root;
+
+  let count = 1;
+  if (typeof requestedCount === 'number' && requestedCount >= 0) {
+    count = Math.floor(requestedCount);
   }
 
   for (let i = 0; i < count; i += 1) {
-    buildElement('p', [], `Paragraph ${i}`, $articleSectionBody);
+    buildElement('p', [], `Paragraph ${i + 1}`, $articleSectionBody);
   }
 
   return $root;
