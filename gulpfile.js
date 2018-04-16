e3ecb15a0227dd87bfd4a8bf17e388fcd0acaddf
Giorgio Sironi <g.sironi@elifesciences.org>
2018-04-16 14:50:55 +0100

    Only execute browserify-tests in `gulp test`

--- gulpfile.js
+++ gulpfile.js
@@ -177,7 +177,7 @@ gulp.task('fonts', () => {
  * Creates a sourcemap.
  ******************************************************************************/
 
-gulp.task('js', ['js:hint', 'js:cs', 'browserify-tests'], () => {
+gulp.task('js', ['js:hint', 'js:cs'], () => {
 
     return browserify('./assets/js/main.js', { debug: true })
           .transform(babel)
