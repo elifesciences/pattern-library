9db9f29864aadc7fb35bdee779e07469a4548ee3
David Moulton <d.moulton@elifesciences.org>
2018-05-02 16:50:44 +0100

    Apply disabled Hypothesis trigger one level up where necessary

--- assets/js/components/SpeechBubble.js
+++ assets/js/components/SpeechBubble.js
@@ -40,14 +40,20 @@ module.exports = class SpeechBubble {
     this.$elm.classList.remove('speech-bubble--loading');
   }
 
-  showFailureState(includePreviousSibling) {
+  showFailureState(applyToParent) {
     this.removeLoadingIndicator();
     this.$elm.setAttribute('disabled', 'disabled');
     this.$elm.style.cursor = 'not-allowed';
 
-    if (includePreviousSibling && this.$elm.previousElementSibling) {
-      this.$elm.previousElementSibling.style.cursor = 'not-allowed';
-      this.$elm.previousElementSibling.style.color = '#888'; // $color-text-secondary
+    if (applyToParent) {
+      this.$elm.parentElement.style.cursor = 'not-allowed';
+      this.$elm.parentElement.style.color = '#888'; // $color-text-secondary
+
+      if (this.$elm.previousElementSibling) {
+        this.$elm.previousElementSibling.style.cursor = 'not-allowed';
+        this.$elm.previousElementSibling.style.color = '#888'; // $color-text-secondary
+      }
+
     }
   }
 
