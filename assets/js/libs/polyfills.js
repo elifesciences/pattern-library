048e510b9367b2f2831b44c4bf201f141a52e4e5
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-06-12 10:55:13 +0100

    Use external polyfill

--- assets/js/libs/polyfills.js
+++ assets/js/libs/polyfills.js
@@ -5,57 +5,24 @@ require('core-js/es6/set');
 require('core-js/fn/object/assign');
 require('core-js/fn/symbol');
 require('core-js/fn/symbol/iterator');
+require('element-closest');
 
 module.exports = (function polyfills() {
 
   (function () {
 
-    const ElementProto = window.Element.prototype;
+    if ( typeof window.CustomEvent === "function" ) return false;
 
-    if (typeof ElementProto.matches !== 'function') {
-      ElementProto.matches = ElementProto.msMatchesSelector || ElementProto.mozMatchesSelector || ElementProto.webkitMatchesSelector || function matches(selector) {
-        var element = this;
-        var elements = (element.document || element.ownerDocument).querySelectorAll(selector);
-        var index = 0;
-
-        while (elements[index] && elements[index] !== element) {
-          ++index;
-        }
-
-        return Boolean(elements[index]);
-      };
-    }
-
-    if (typeof ElementProto.closest !== 'function') {
-      ElementProto.closest = function closest(selector) {
-        var element = this;
-
-        while (element && element.nodeType === 1) {
-          if (element.matches(selector)) {
-            return element;
-          }
-
-          element = element.parentNode;
-        }
-
-        return null;
-      };
-    }
-
-    function CustomEvent(event, params) {
+    function CustomEvent ( event, params ) {
       params = params || { bubbles: false, cancelable: false, detail: undefined };
-      var evt = document.createEvent('CustomEvent');
-      evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+      var evt = document.createEvent( 'CustomEvent' );
+      evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
       return evt;
     }
 
-    if ( typeof window.CustomEvent !== "function" ) {
+    CustomEvent.prototype = window.Event.prototype;
 
-      CustomEvent.prototype = window.Event.prototype;
-
-      window.CustomEvent = CustomEvent;
-    }
-
+    window.CustomEvent = CustomEvent;
   })();
 
 }());
