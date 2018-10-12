ee7602e06ab7dc3e7613fe3ad605433be3ddbde7
Dave Price <d.price@elifesciences.org>
2018-10-12 14:04:32 +0100

    Add test for visuallyhidden

--- test/articledownloadlinkslist.spec.js
+++ test/articledownloadlinkslist.spec.js
@@ -29,7 +29,13 @@ describe('An ArticleDownloadLinksList Component', function () {
     it('Sets aria-expanded attribute to "true"', function () {
       articleDownloadLinksList.open();
       expect(articleDownloadLinksList.$elm.setAttribute.calledWithExactly('aria-expanded', true)).to.be.true;
+    })
+
+    it('Sets class "visuallyhidden" to be removed', function () {
+      articleDownloadLinksList.open();
+      expect(articleDownloadLinksList.$elm.classList.contains('visuallyhidden')).to.be.false;
     });
+
 
   });
 
@@ -38,6 +44,11 @@ describe('An ArticleDownloadLinksList Component', function () {
     it('Sets aria-expanded attribute to "false"', function () {
       articleDownloadLinksList.close();
       expect(articleDownloadLinksList.$elm.setAttribute.calledWithExactly('aria-expanded', false)).to.be.true;
+    });
+
+    it('Sets class "visuallyhidden" to be added', function () {
+      articleDownloadLinksList.close();
+      expect(articleDownloadLinksList.$elm.classList.contains('visuallyhidden')).to.be.true;
     });
 
   });
