e64f9338b1a7352bc7d70805333f88152ede8d51
Dave Price <d.price@elifesciences.org>
2018-10-12 09:32:59 +0100

    Add test for aria-expanded

--- test/articledownloadlinkslist.spec.js
+++ test/articledownloadlinkslist.spec.js
@@ -1,4 +1,10 @@
-let expect = chai.expect;
+const chai = require('chai');
+const sinon = require('sinon');
+
+const expect = chai.expect;
+const spy = sinon.spy;
+
+let utils = require('../assets/js/libs/elife-utils')();
 
 // load in component(s) to be tested
 let ArticleDownloadLinksList = require('../assets/js/components/ArticleDownloadLinksList');
@@ -16,5 +22,28 @@ describe('An ArticleDownloadLinksList Component', function () {
   it('exists', function () {
     expect(articleDownloadLinksList).to.exist;
   });
+
+});
+
+describe('ArticleDownloadLinksList Aria Expanded Test', function () {
+
+  let generateAria
+
+  beforeEach(function () {
+    spy(window, "matchMedia");
+  });
+
+  afterEach(function () {
+    window.matchMedia.restore();
+  });
+
+  context('ArticleDownloadLinksList is closed', function () {
+
+    it('returns false', function () {
+      expect(window.matchMedia.calledWithExactly('aria-expanded="false"'));
+    });
+
+  });
+
 
 });
