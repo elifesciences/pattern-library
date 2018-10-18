f9021319dd5a557cb238be1fda78ad3096f9e4ba
Dave Price <d.price@elifesciences.org>
2018-10-18 13:48:23 +0100

    Add tests for main menu

--- test/mainmenu.spec.js
+++ test/mainmenu.spec.js
@@ -1,4 +1,8 @@
-let expect = chai.expect;
+const chai = require('chai');
+const sinon = require('sinon');
+
+const expect = chai.expect;
+const spy = sinon.spy;
 
 // load in component(s) to be tested
 let MainMenu = require('../assets/js/components/MainMenu');
@@ -11,16 +15,57 @@ describe('A MainMenu Component', function () {
   beforeEach(function () {
     $elm = document.querySelector('[data-behaviour="MainMenu"]');
     mainMenu = new MainMenu($elm);
+    mainMenu.pageOverlay = {
+      show: function () {
+
+      },
+      get$elm: function () {
+        return {
+          addEventListener: function () {
+
+          }
+        };
+      }
+    };
+    spy(mainMenu.$elm, 'setAttribute');
   });
 
   afterEach(function () {
-    $elm = null;
-    mainMenu = null;
+    mainMenu.$elm.setAttribute.restore();
   });
+
 
   it('exists', function () {
     expect(mainMenu).to.exist;
   });
 
+
+  describe('The open method', function () {
+
+    it('sets aria-expanded attribute to "true"', function () {
+      mainMenu.open();
+      expect(mainMenu.$elm.setAttribute.calledWithExactly('aria-expanded', 'true')).to.be.true;
+    });
+
+    it('adds the class "main-menu--shown"', function () {
+      mainMenu.open();
+      expect(mainMenu.$elm.classList.contains('main-menu--shown')).to.be.true;
+    });
+
+  });
+
+  describe('The close method', function () {
+
+    it('sets aria-expanded attribute to "false"', function () {
+      //mainMenu.close();
+      expect(mainMenu.$elm.setAttribute.calledWithExactly('aria-expanded', 'false')).to.be.false;
+    });
+
+    it('removes the class "main-menu--shown"', function () {
+      mainMenu.$elm.classList.remove('main-menu--shown');
+      expect(mainMenu.$elm.classList.contains('main-menu--shown')).to.be.false;
+    });
+
+  });
 
 });
