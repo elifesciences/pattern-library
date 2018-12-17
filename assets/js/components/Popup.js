56e1811f9a6c40450afb3e5faa2cc9761720cb67
Dave <discodavey@gmail.com>
2018-12-17 10:57:45 +0000

    Add aria-expanded, hidden and focus (#930)
    * Add aria-expanded, hidden and focus
    
    * Move popup__wrapper in dom to sit before $link
    
    This is for screen readers to read and tab correctly
    
    * Refactor code
    
    * Refactored code for tabbing issues
    
    * Refactor code so tabbing works correctly
    
    * Refactor code for new method
    
    * Refactor code

--- assets/js/components/Popup.js
+++ assets/js/components/Popup.js
@@ -234,6 +234,28 @@ module.exports = class Popup {
     };
   }
 
+  setAccessibilityAttributesPopupHitBox(isToBeShown) {
+    if (isToBeShown) {
+      this.popupHitBox.setAttribute('aria-expanded', 'true');
+      this.popupHitBox.setAttribute('tabindex', -1);
+      this.popupHitBox.setAttribute('aria-hidden', 'false');
+      this.popupHitBox.focus();
+    } else {
+      this.popupHitBox.setAttribute('aria-expanded', 'false');
+      this.popupHitBox.removeAttribute('tabindex', -1);
+      this.popupHitBox.setAttribute('aria-hidden', 'true');
+      this.popupHitBox.style.display = 'none';
+    }
+  }
+
+  setAttributesForOpenPopupHitBox() {
+    this.setAccessibilityAttributesPopupHitBox(true);
+  }
+
+  setAttributesForClosedPopupHitBox() {
+    this.setAccessibilityAttributesPopupHitBox(false);
+  }
+
   positionPopupHitBox(e) {
     this.popupHitBox.style.display = '';
 
@@ -260,8 +282,10 @@ module.exports = class Popup {
       top = topIfAbove;
     }
 
+    this.$link.parentNode.insertBefore(this.popupHitBox, this.$link);
     this.popupHitBox.style.left = `${left}px`;
     this.popupHitBox.style.top = `${top}px`;
+    this.setAttributesForOpenPopupHitBox();
   }
 
   static setLinksClasses($root) {
@@ -318,8 +342,16 @@ module.exports = class Popup {
     // Changing the state depending on the other properties of the object.
     if (this.isOpen) {
       this.positionPopupHitBox(e);
+
+      this.popupHitBox.addEventListener('blur', () => {
+        this.setAttributesForClosedPopupHitBox();
+        this.isOpen = false;
+        this.$link.focus();
+      });
+
     } else {
-      this.popupHitBox.style.display = 'none';
+      this.setAttributesForClosedPopupHitBox();
+      this.$link.focus();
     }
 
     return Promise.resolve(this.$link);
