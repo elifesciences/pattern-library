3856d12f35831af92ae0e05dd93d7a377d090976
Dave Price <discodavey@gmail.com>
2019-12-12 12:05:31 +0000

    deriveCookieName for DismissButton

--- assets/js/components/DismissButton.js
+++ assets/js/components/DismissButton.js
@@ -7,6 +7,21 @@ module.exports = class DismissButton {
     this.$elm = $elm;
     this.window = _window;
     this.doc = doc;
+
+    this.cookieName = DismissButton.deriveCookieName(this.$elm);
+    // if (!this.cookieName.length || this.hasAlreadyBeenSeen(this.cookieName)) {
+    //   this.hide();
+    //   return;
+    // }
+  }
+
+  static deriveCookieName($elm) {
+    const cookieNameRoot = $elm.dataset.cookieNameRoot;
+    if ($elm.id && $elm.id.length && cookieNameRoot && cookieNameRoot.length) {
+      return `${cookieNameRoot}${$elm.id}`;
+    }
+
+    return '';
   }
 
   buildButton() {
