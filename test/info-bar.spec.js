f98001a37050b3307fd3c51c2234044ad89a5124
Dave Price <d.price@elifesciences.org>
2020-01-02 10:22:51 +0000

    Add infobar tests

---
+++ test/info-bar.spec.js
@@ -0,0 +1,7 @@
+const utils = require('../assets/js/libs/elife-utils')();
+const chai = require('chai');
+
+const expect = chai.expect;
+
+// load in component(s) to be tested
+const CallToAction = require('../assets/js/components/CallToAction');
