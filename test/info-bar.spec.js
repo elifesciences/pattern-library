d9c70566c7e92382b0ba45a7085b8f285582ae44
David Moulton <d.moulton@elifesciences.org>
2020-01-07 16:25:19 +0000

    Add some tests for dismissible info bar

--- test/info-bar.spec.js
+++ test/info-bar.spec.js
@@ -1,31 +1,159 @@
-const utils = require('../assets/js/libs/elife-utils')();
 const chai = require('chai');
+const InfoBar = require('../assets/js/components/InfoBar');
+const utils = require('../assets/js/libs/elife-utils')();
 
 const expect = chai.expect;
 
-// load in component(s) to be tested
-const InfoBar = require('../assets/js/components/InfoBar');
-
-//If there is not a cookie name then dismiss.hide is called
-//If there is a cookie name and does not have a value of true then dismiss.hide is not called
-
-
-describe('An InfoBar Component', function () {
+function setFixtureCookie(value) {
   'use strict';
 
-  it ('calls dismiss.hide if there is not a cookie name', function() {
-    const $noId = utils.buildElement('div', ['info-bar', 'info-bar--dismissible'], '',  '.fixture-container');
-    $noId.dataset.cookieNameRoot = 'theRootOfTheCookieName_';
-    const infoBarNoId = new InfoBar($noId);
+  const nameRoot = 'fixture-cookie_';
+  const id = 'fixtureId';
+  const name = `${nameRoot}${id}`;
 
-    const $noCookieNameRoot = utils.buildElement('div', ['info-bar', 'info-bar--dismissible'], '',  '.fixture-container');
-    $noCookieNameRoot.setAttribute('id', 'idWhenNoCookieNameRoot');
-    const infoBarNoCookieNameRoot = new InfoBar($noCookieNameRoot);
+  document.cookie= `${name}=${value}; expires=Tue, 19 January 2038 03:14:07 UTC; path=/;`;
 
-    const $noIdNorCookieNameRoot = utils.buildElement('div', ['info-bar', 'info-bar--dismissible'], '',  '.fixture-container');
-    const infoBarNoIdNorCookieNameRoot = new InfoBar($noIdNorCookieNameRoot);
+  return {
+    nameRoot,
+    id,
+  };
+}
 
-    // const infoBar = new InfoBar($elm);
+function clearFixtureCookie() {
+  'use strict';
+
+  document.cookie = `fixture-cookie_fixtureId=false; expires=expires=Thu, 01 Jan 1970 00:00:01 UTC; path=/;`;
+}
+
+describe('A dismissible InfoBar Component', function () {
+  'use strict';
+
+  let $infoBar;
+
+  beforeEach(function () {
+    $infoBar = utils.buildElement(
+      'div',
+      ['info-bar',
+       'info-bar--dismissible'],
+      '',
+      '.fixture-container'
+    );
+    $infoBar.dataset.generatedFixture = '';
+  });
+
+  afterEach(function () {
+    [].slice.call(document.querySelectorAll('[data-generated-fixture]')).forEach(function (node) {
+      node.parentNode.removeChild(node);
+    });
+    $infoBar = null;
+
+    clearFixtureCookie();
+  });
+
+  describe('has a cookie', function () {
+
+    describe('with a name', function () {
+
+      it('that is derived from the HTML element\'s id and data-cookie-name-root attribute  ', function () {
+        $infoBar.dataset.cookieNameRoot = 'some-cookie-name-root_';
+        $infoBar.setAttribute('id', 'some-id');
+
+        const infoBar = new InfoBar($infoBar);
+        expect(infoBar.dismiss.cookieName).to.equal('some-cookie-name-root_some-id');
+      });
+
+      it('that is empty if the HTML element lacks an id', function () {
+        $infoBar.dataset.cookieNameRoot = 'some-cookie-name-root';
+
+        const infoBar = new InfoBar($infoBar);
+        expect(infoBar.dismiss.cookieName).to.equal('');
+      });
+
+      it('that is empty if the HTML element lacks a data-cookie-name-root attribute', function () {
+        $infoBar.setAttribute('id', 'some-id');
+
+        const infoBar = new InfoBar($infoBar);
+        expect(infoBar.dismiss.cookieName).to.equal('');
+      });
+
+      it('that when empty does not hide the info bar', function() {
+        const infoBar = new InfoBar($infoBar);
+        expect(infoBar.$elm.classList.contains('hidden')).to.be.false;
+      });
+
+    });
+
+    describe('with an expiry date', function () {
+
+      context('that when the HTML attribute data-cookie-expires has a value', function () {
+
+        it('is set to that date', function () {
+          debugger;
+          expect(utils.getCookieValue('fixture-cookie_fixtureId', document.cookie), 'cookie shouldn\'t be set yet').to.equal('');
+          const expectedExpiry = 'Tue, 19 January 2038 03:14:07 UTC';
+          $infoBar.dataset.cookieExpires = expectedExpiry;
+          new InfoBar($infoBar);
+          expect(utils.getCookieValue('fixture-cookie_fixtureId', document.cookie), 'cookie not set').to.equal(true);
+
+          let actualExpiry = '';
+          document.cookie.split('; ').forEach((cookie) => {
+            if (cookie.indexOf('fixture-cookie_fixtureId') === 0) {
+              const re = new RegExp(`expiry=([^;]*)`);
+              const cookieMatch = cookie.match(re)[1];
+              actualExpiry = decodeURIComponent(cookieMatch);
+            }
+          });
+
+          expect(actualExpiry).to.equal(expectedExpiry);
+
+        });
+
+      });
+
+      context('that when the HTML attribute data-cookie-duration is set as an integer', function () {
+
+        xit('is set to that date that number of days in the future');
+
+      });
+
+    });
+
+    describe('that if already exists with the value "true"', function () {
+
+      let cookieNameParts;
+
+      beforeEach(function() {
+        cookieNameParts = setFixtureCookie('true');
+      });
+
+      it('hides the info bar', function () {
+        $infoBar.setAttribute('id', cookieNameParts.id);
+        $infoBar.dataset.cookieNameRoot = cookieNameParts.nameRoot;
+        const infoBar = new InfoBar($infoBar);
+
+        expect(infoBar.$elm.classList.contains('hidden')).to.be.true;
+      });
+
+    });
+
+    describe('that if already exists with a value that is not "true"', function () {
+
+      let cookieNameParts;
+
+      beforeEach(function() {
+        cookieNameParts = setFixtureCookie('false');
+      });
+
+      it('doesn\'t hide the info bar', function () {
+        $infoBar.setAttribute('id', cookieNameParts.id);
+        $infoBar.dataset.cookieNameRoot = cookieNameParts.nameRoot;
+        const infoBar = new InfoBar($infoBar);
+
+        expect(infoBar.$elm.classList.contains('hidden')).to.be.false;
+      });
+
+    });
+
   });
 
 });
