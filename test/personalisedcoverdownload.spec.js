43db88411316a16b9ad303ed6aabd49dec8b9ba8
nlisgo <nathan@aeopus.com>
2020-09-30 19:40:39 +0100

    Expand tests

--- test/personalisedcoverdownload.spec.js
+++ test/personalisedcoverdownload.spec.js
@@ -3,18 +3,52 @@ let expect = chai.expect;
 // load in component(s) to be tested
 let PersonalisedCoverDownload = require('../assets/js/components/PersonalisedCoverDownload');
 
-describe('A PersonalisedCoverDownload Component', function () {
+describe('A PersonalisedCoverDownload Component', () => {
   'use strict';
 
   let $elm = document.querySelector('[data-behaviour="PersonalisedCoverDownload"]');
   let personalisedCoverDownload;
 
-  beforeEach(function () {
+  beforeEach(() => {
     personalisedCoverDownload = new PersonalisedCoverDownload($elm, window, window.document);
   });
 
-  it('exists', function () {
+  it('exists', () => {
     expect(personalisedCoverDownload).to.exist;
+  });
+
+  describe('the checkbox/button collections interaction', () => {
+    let a4ButtonCollection;
+    let letterButtonCollection;
+    let checkbox;
+
+    before(function () {
+      a4ButtonCollection = document.querySelector('.button-collection--a4');
+      letterButtonCollection = document.querySelector('.button-collection--usletter');
+      checkbox = this.$elm.querySelector('input');
+    });
+
+    context('before clicking the checkbox', () => {
+      it('the A4 download menu is not hidden', function () {
+        expect(a4ButtonCollection.getAttribute('class')).not.to.contain('visuallyhidden');
+      });
+
+      it('the US letter download menu is hidden', function () {
+        expect(letterButtonCollection.getAttribute('class')).to.contain('visuallyhidden');
+      });
+    });
+
+    context('after clicking the checkbox', () => {
+      // Simulate checkbox change.
+
+      it('the A4 download menu is hidden', function () {
+        expect(a4ButtonCollection.getAttribute('class')).to.contain('visuallyhidden');
+      });
+
+      it('the US letter download menu is not hidden', function () {
+        expect(letterButtonCollection.getAttribute('class')).not.to.contain('visuallyhidden');
+      });
+    });
   });
 
 });
