751b7a06bfd6703f0cb9072df4ed2793be9686e7
Dave Price <d.price@elifesciences.org>
2020-12-21 14:12:16 +0000

    Create new grid listing as breakpoint has changed on About profiles

--- assets/js/components/AboutProfiles.js
+++ assets/js/components/AboutProfiles.js
@@ -11,11 +11,8 @@ module.exports = class AboutProfiles {
     this.window = _window;
     this.doc = doc;
 
-    this.$elm.classList.add('grid-listing');
+    this.$elm.classList.add('grid-listing__about');
     this.$elm.classList.add('clearfix'); // IE doesn't support multiple arguments
-    [].forEach.call(this.$elm.querySelectorAll('li'), (item) => {
-      item.classList.add('grid-listing-item');
-    });
   }
 
 };
