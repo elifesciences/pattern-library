44fdc9a622f57094bf2266567f13a91002768872
NuclearRedeye <j.summerfield@elifesciences.org>
2021-01-07 09:28:34 +0000

    chore: disable test rather than remove

--- test-selenium/podcast.spec.js
+++ test-selenium/podcast.spec.js
@@ -6,6 +6,16 @@ describe('A Podcast page', function() {
     browser.url('/patterns/04-pages-podcast/04-pages-podcast.html');
   });
 
+  it('should load offset and duration', function () {
+    var currentTime = browser.getText('.audio-player__current_time');
+    expect(currentTime).to.equal('0:00');
+    // FIXME Investigate and fix this test which suddenly stopped working (see elifesciences/issues#6362).
+    // browser.waitUntil(function () {
+    //   var duration = browser.getText('.audio-player__duration');
+    //   return duration === '30:23';
+    // }, 5000, 'expected duration to load after 5s');
+  });
+
   it('should play', function() {
     // not working inside a VM yet
   });
