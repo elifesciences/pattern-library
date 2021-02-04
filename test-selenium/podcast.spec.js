e83953b381e28211c4ec17590cc1f2c9e868d689
Joel Summerfield <52923935+NuclearRedeye@users.noreply.github.com>
2021-02-04 17:57:34 +0000

    chore: skip podcast tests (#1055)

--- test-selenium/podcast.spec.js
+++ test-selenium/podcast.spec.js
@@ -1,7 +1,8 @@
 let chai = require('chai');
 let expect = chai.expect;
 
-describe('A Podcast page', function() {
+// FIXME Investigate and fix this test which suddenly stopped working (see elifesciences/issues#6362).
+describe.skip('A Podcast page', function() {
   it('should have the right title', function () {
     browser.url('/patterns/04-pages-podcast/04-pages-podcast.html');
   });
@@ -9,11 +10,10 @@ describe('A Podcast page', function() {
   it('should load offset and duration', function () {
     var currentTime = browser.getText('.audio-player__current_time');
     expect(currentTime).to.equal('0:00');
-    // FIXME Investigate and fix this test which suddenly stopped working (see elifesciences/issues#6362).
-    // browser.waitUntil(function () {
-    //   var duration = browser.getText('.audio-player__duration');
-    //   return duration === '30:23';
-    // }, 5000, 'expected duration to load after 5s');
+    browser.waitUntil(function () {
+      var duration = browser.getText('.audio-player__duration');
+      return duration === '30:23';
+    }, 5000, 'expected duration to load after 5s');
   });
 
   it('should play', function() {
