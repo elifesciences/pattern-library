2e7cba7e1c5898e0ec6e5facf9545e4046112b03
Dave Price <d.price@elifesciences.org>
2021-04-08 13:58:04 +0100

    Removing class

--- assets/js/components/Popup.js
+++ assets/js/components/Popup.js
@@ -191,7 +191,7 @@ module.exports = class Popup {
       const $ancillary = utils.buildElement('div', ['popup__content__ancillary']);
       const exclusions = ['reference__title', 'reference__authors_list',
                           'reference__authors_list_suffix', 'author-details__name',
-                          'about-profile__name', 'about-profile__role', 'table__table'];
+                          'about-profile__name', 'about-profile__role'];
       const ancillaries = [];
 
       // If an immediate child of content does not have an excluded class, it's ancillary
