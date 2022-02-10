bc0d195fa3e6cff5e6289dcd626daf17cf965695
nlisgo <nathan@aeopus.com>
2022-02-10 22:17:53 +0000

    React to checked and unchecked state

--- assets/js/components/HiddenUntilChecked.js
+++ assets/js/components/HiddenUntilChecked.js
@@ -14,6 +14,21 @@ module.exports = class HiddenUntilChecked {
     const $checkbox = this.buildCheckbox(this.$elm.querySelector('label').textContent);
 
     this.$elm.parentNode.insertBefore($checkbox, this.$elm);
+
+    const $checkboxInput = $checkbox.querySelector('input');
+    const $textInput = this.$elm.querySelector('input');
+
+    $checkboxInput.addEventListener('change', e => {
+      if (e.target.checked) {
+        this.$elm.classList.remove('visuallyhidden');
+        $textInput.focus();
+      } else {
+        this.$elm.classList.add('visuallyhidden');
+        $textInput.value = '';
+      }
+    });
+
+    this.$elm.classList.add('visuallyhidden');
   }
 
   buildCheckbox(labelText) {
