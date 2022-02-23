1b9e25fb7c8759e4943983939d768cc73fdfd73f
nlisgo <nathan@aeopus.com>
2022-02-23 13:05:38 +0000

    Introduce placeholder for test for button clipboard

---
+++ test/buttonclipboard.spec.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+
+// load in component(s) to be tested
+const ButtonClipboard = require('../assets/js/components/ButtonClipboard');
+
+describe('A button can be used to store text in clipboard', () => {
+  'use strict';
+
+  let $elm;
+
+  beforeEach(() =>  {
+    $elm = document.querySelector('[data-behaviour="ButtonClipboard"]');
+    new ButtonClipboard($elm);
+  });
+
+  it('stores text into clipboard', () => {
+    // write test to check button clipboard here.
+    expect(true).to.equal(false);
+  });
+
+});
