52718f26e23e867b38582d85f214107d30d2de79
nlisgo <nathan@aeopus.com>
2022-02-24 23:11:39 +0000

    Introduce test for Modal.js

---
+++ test/modalwindow.spec.js
@@ -0,0 +1,39 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+
+// load in component(s) to be tested
+const ModalWindow = require('../assets/js/components/Modal');
+
+describe('modal window', () => {
+  'use strict';
+
+  let $elm;
+  let $elmWithTriggerId;
+  let $elmTrigger;
+  let $elmWithTriggerIdTrigger;
+
+  beforeEach(() => {
+    $elm = document.getElementById('without-trigger-id').querySelector('[data-behaviour="Modal"]');
+    new ModalWindow($elm);
+
+    $elmWithTriggerId = document.getElementById('with-trigger-id').querySelector('[data-behaviour="Modal"]');
+    new ModalWindow($elmWithTriggerId);
+
+    $elmTrigger = document.getElementById('without-trigger-id').querySelector('.modal-click');
+    $elmWithTriggerIdTrigger = document.getElementById('trigger-id');
+  });
+
+  afterEach(() => {
+    $elm.querySelector('.modal-container').classList.remove('modal-content__show');
+    $elmWithTriggerId.querySelector('.modal-container').classList.remove('modal-content__show');
+    $elm.classList.add('modal-nojs');
+    $elmWithTriggerId.classList.add('modal-nojs');
+  });
+
+  it('modal is initially hidden', () => {
+    expect($elm.classList.contains('modal-content__show')).to.be.false;
+    expect($elmWithTriggerId.classList.contains('modal-content__show')).to.be.false;
+  });
+
+});
