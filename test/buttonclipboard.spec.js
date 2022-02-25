3f646d3fc8f6f43aa9eb5a0c1c99b80826148c5a
nlisgo <nathan@aeopus.com>
2022-02-25 11:56:37 +0000

    expand tests

--- test/buttonclipboard.spec.js
+++ test/buttonclipboard.spec.js
@@ -9,21 +9,20 @@ const ButtonClipboard = require('../assets/js/components/ButtonClipboard');
 describe('A button can be used to store text in clipboard', () => {
   'use strict';
 
-  let $elm;
+  let $elm = document.querySelector('[data-behaviour="ButtonClipboard"]');
   let $clipboardText;
+  let $btnClipboard;
 
   beforeEach(() =>  {
-    $elm = document.querySelector('[data-behaviour="ButtonClipboard"]');
     $clipboardText = '';
-    const btnClipboard = new ButtonClipboard($elm, window, window.document);
-    sinon.stub(btnClipboard, 'supportsClipboardAPI').callsFake(() => false);
-    sinon.stub(btnClipboard, 'copyToClipboardFallback').callsFake((text, onSuccess) => {
-        $clipboardText = text;
-        onSuccess();
-    });
+    $btnClipboard = new ButtonClipboard($elm, window, window.document);
+    sinon.stub($btnClipboard, 'supportsClipboardAPI').callsFake(() => false);
   });
 
   it('changes button when clipboard triggered', () => {
+    sinon.stub($btnClipboard, 'copyToClipboardFallback').callsFake((text) => {
+        $clipboardText = text;
+    });
     expect($elm.textContent).to.equal('Button clipboard');
     expect($elm.classList.contains('button--success')).to.be.false;
     expect($clipboardText).to.be.empty;
@@ -31,6 +30,16 @@ describe('A button can be used to store text in clipboard', () => {
     expect($elm.textContent).to.equal('Copied!');
     expect($elm.classList.contains('button--success')).to.be.true;
     expect($clipboardText).to.equal('Text to store in clipboard');
+  });
+
+  it('disables button if copy fails', () => {
+    sinon.stub($btnClipboard, 'copyToClipboardFallback').callsFake((text) => {
+        throw new Error('copy failed');
+    });
+    $elm.click();
+    expect($elm.textContent).to.equal('Copy not supported');
+    expect($elm.classList.contains('button--fail')).to.be.true;
+    expect($elm.disabled).to.be.true;
   });
 
 });
