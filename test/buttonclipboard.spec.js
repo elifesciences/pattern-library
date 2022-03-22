2f733b7fe36e3514d1b8a299187d5052b2e60197
nlisgo <nathan@aeopus.com>
2022-03-22 21:43:34 +0000

    Trigger button clipboard reset when modal closes

--- test/buttonclipboard.spec.js
+++ test/buttonclipboard.spec.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const sinon = require('sinon');
+const utils = require('../assets/js/libs/elife-utils')();
 
 const expect = chai.expect;
 
@@ -10,6 +11,7 @@ describe('A button can be used to store text in clipboard', () => {
   'use strict';
 
   let $elm = document.querySelector('[data-behaviour="ButtonClipboard"]');
+  let $modal = document.querySelector('.modal');
   let $clipboardText;
   let $btnClipboard;
 
@@ -42,6 +44,20 @@ describe('A button can be used to store text in clipboard', () => {
     expect($elm.textContent).to.equal('Copy not supported');
     expect($elm.classList.contains('button--fail')).to.be.true;
     expect($elm.disabled).to.be.true;
+  });
+
+  it('resets button when modalWindowClose triggered', () => {
+    sinon.stub($btnClipboard, 'copyToClipboardFallback').callsFake((text) => {
+      $clipboardText = text;
+    });
+    expect($elm.textContent).to.equal('Button clipboard');
+    expect($elm.classList.contains('button--success')).to.be.false;
+    $elm.click();
+    expect($elm.textContent).to.equal('Copied!');
+    expect($elm.classList.contains('button--success')).to.be.true;
+    $modal.dispatchEvent(utils.eventCreator('modalWindowClose'));
+    expect($elm.textContent).to.equal('Button clipboard');
+    expect($elm.classList.contains('button--success')).to.be.false;
   });
 
 });
