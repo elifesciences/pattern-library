4ba1f5427276a5adb5626295fb4358f111597f09
nlisgo <nathan@aeopus.com>
2022-04-08 22:00:37 +0100

    Introduce test

---
+++ test/readcubecheck.spec.js
@@ -0,0 +1,25 @@
+const expect = chai.expect;
+
+// CheckPMC module
+const CheckPMC = require('../assets/js/components/CheckPMC');
+
+describe('Check PMC integration', function () {
+  'use strict';
+  const root = {
+    checkPMC: null,
+    $elm: null
+  };
+
+  // Mocks
+  CheckPMC.prototype.checkPMC = url => {
+    return Promise.resolve((url !== 'broken'));
+  };
+
+  root.$elm = document.querySelector('[data-behaviour="CheckPMC"]');
+  root.checkPMC = new CheckPMC(root.$elm, window, window.document);
+
+  it('exists', function () {
+    expect(root.checkPMC).to.exist;
+  });
+
+});
