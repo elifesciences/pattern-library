8351dd9f7c3d83577d335d5484848c376bae579c
Viktoria Nedyalkova <viktoria.nedyalkova@endava.com>
2022-08-31 09:52:49 +0300

    Revert changes

--- gulpfile.js
+++ gulpfile.js
@@ -172,7 +172,7 @@ gulp.task('fonts', () => {
  * Creates a sourcemap.
  ******************************************************************************/
 
-gulp.task('js', ['js:hint', 'js:copyLoader'], () => {
+gulp.task('js', ['js:hint', 'js:cs', 'js:copyLoader'], () => {
 
     return browserify('./assets/js/main.js', { debug: true })
           .transform(babel)
