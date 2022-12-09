2faa1ad052569072d0c9466fafa6452b811e95fd
Cosmin-Daniel Urzicianu <cosmindaniel.urzicianu@endava.com>
2022-12-09 13:48:57 +0200

    add resize functionality

--- assets/js/components/Collapsible.js
+++ assets/js/components/Collapsible.js
@@ -25,13 +25,15 @@ module.exports = class Collapsible {
       this.$maxVisibleCollapsedElements = this.maxVisibleCollapsedElementsOnMediumViewport;
       this.initialise();
     }
+
+    this.window.addEventListener('resize', this.initialiseOnResize.bind(this));
   }
 
   initialise() {
     this.$elm.classList.add('collapsible--js');
     this.$toggle = this.createToggle();
     this.$collapsibleElements = this.listElements.slice(this.$maxVisibleCollapsedElements, this.listElements.length);
-    this.setInitialState(this.$toggle, this.$collapsibleElements);
+    this.setInitialState();
     this.$elm.addEventListener('expandsection', this.expand.bind(this));
     this.$elm.addEventListener('collapsesection', this.collapse.bind(this));
   }
@@ -47,7 +49,7 @@ module.exports = class Collapsible {
     return $link;
   }
 
-  setInitialState($toggle, $collapsibleElements) {
+  setInitialState() {
     if (this.window.navigator.userAgent && this.window.navigator.userAgent.indexOf('Googlebot/') !== -1) {
       // Google Scholar requires article timeline to be open to improve indexing
       this.$elm.dataset.initialState = 'opened';
@@ -59,19 +61,50 @@ module.exports = class Collapsible {
       this.$elm.dataset.initialState = 'closed';
     }
 
+    this.toggleContent();
+  }
+
+  toggleContent() {
     if (this.$elm.dataset.initialState === 'closed') {
       this.$elm.classList.add('toggle--collapsed');
-      $toggle.classList.add('toggle--closed');
-      $collapsibleElements.forEach(function (elem) {
+      this.$toggle.classList.add('toggle--closed');
+      this.listElements.forEach(function (elem) {
+        elem.classList.remove('visuallyhidden');
+      });
+
+      this.$collapsibleElements.forEach(function (elem) {
         elem.classList.add('visuallyhidden');
       });
     } else {
       // Defensive: remove classes if they turn up here when they shouldn't
-      $toggle.classList.remove('toggle--closed');
-      $collapsibleElements.forEach(function (elem) {
+      this.$toggle.classList.remove('toggle--closed');
+
+      this.listElements.forEach(function (elem) {
         elem.classList.remove('visuallyhidden');
       });
     }
+  }
+
+  initialiseOnResize() {
+    if (!this.viewportNoWiderThan(this.viewportWidthLarge)) {
+      this.$toggle.classList.add('visuallyhidden');
+      return;
+    }
+
+    this.$toggle.classList.remove('visuallyhidden');
+
+    if (this.viewportNoWiderThan(this.viewportWidthMedium) &&
+      this.listElements.length > this.maxVisibleCollapsedElementsOnSmallViewport) {
+      this.$maxVisibleCollapsedElements = this.maxVisibleCollapsedElementsOnSmallViewport;
+      this.$toggle.innerHTML = 'History';
+    } else if (this.viewportNoWiderThan(this.viewportWidthLarge) &&
+      this.listElements.length > this.maxVisibleCollapsedElementsOnMediumViewport) {
+      this.$maxVisibleCollapsedElements = this.maxVisibleCollapsedElementsOnMediumViewport;
+      this.$toggle.innerHTML =  'Show history';
+    }
+
+    this.$collapsibleElements = this.listElements.slice(this.$maxVisibleCollapsedElements, this.listElements.length);
+    this.toggleContent();
   }
 
   viewportNoWiderThan(thresholdInPx) {
@@ -90,6 +123,7 @@ module.exports = class Collapsible {
   collapse() {
     this.$toggle.classList.add('toggle--closed');
     this.$elm.classList.add('toggle--collapsed');
+    this.$elm.dataset.initialState = 'closed';
     this.$toggle.innerHTML =
       ((this.$maxVisibleCollapsedElements === this.maxVisibleCollapsedElementsOnMediumViewport) ? 'Show history' : 'History');
     this.$collapsibleElements.forEach(function (elem) {
@@ -100,6 +134,7 @@ module.exports = class Collapsible {
   expand() {
     this.$toggle.classList.remove('toggle--closed');
     this.$elm.classList.remove('toggle--collapsed');
+    this.$elm.dataset.initialState = 'opened';
     this.$toggle.innerHTML =
       ((this.$maxVisibleCollapsedElements === this.maxVisibleCollapsedElementsOnMediumViewport) ? 'Hide history' : 'Hide');
     this.$collapsibleElements.forEach(function (elem) {
