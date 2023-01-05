b6f293fe477c64498a1d60c81213354a46c82835
Cosmin-Daniel Urzicianu <cosmindaniel.urzicianu@endava.com>
2023-01-05 17:57:01 +0200

    improve JS

--- assets/js/components/ContentAside.js
+++ assets/js/components/ContentAside.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const utils = require('../libs/elife-utils')();
-
 module.exports = class ContentAside {
 
   constructor($elm, _window = window, doc = document) {
@@ -15,21 +13,16 @@ module.exports = class ContentAside {
     this.maxVisibleCollapsedElementsOnSmallViewport = 2;
     this.maxVisibleCollapsedElementsOnMediumViewport = 6;
     this.listElements = Array.prototype.slice.call($elm.querySelector('.definition-list--timeline').children);
-    if (this.viewportNoWiderThan(this.viewportWidthLarge) &&
-      this.listElements.length < this.maxVisibleCollapsedElementsOnMediumViewport) {
-      return;
+
+    if (this.viewportNoWiderThan(this.viewportWidthMedium) &&
+      this.listElements.length > this.maxVisibleCollapsedElementsOnSmallViewport) {
+      this.$toggle = this.createToggle();
+      this.toggleContent();
+    } else if (this.viewportNoWiderThan(this.viewportWidthLarge) &&
+      this.listElements.length > this.maxVisibleCollapsedElementsOnMediumViewport) {
+      this.$toggle = this.createToggle();
+      this.toggleContent();
     }
-
-    this.$toggle = this.createToggle();
-    this.initialise();
-    this.window.addEventListener('resize', this.initialiseOnResize.bind(this));
-  }
-
-  initialise() {
-    this.$elm.classList.add('content-aside--js');
-    this.setInitialState();
-    this.$elm.addEventListener('expandsection', this.expand.bind(this));
-    this.$elm.addEventListener('collapsesection', this.collapse.bind(this));
   }
 
   createToggle() {
@@ -37,49 +30,18 @@ module.exports = class ContentAside {
     $link.setAttribute('href', '#');
     $link.classList.add('toggle');
     this.$elm.appendChild($link);
-    $link.addEventListener('click', this.toggleState.bind(this));
+    $link.addEventListener('click', this.toggleContent.bind(this));
     return $link;
   }
 
-  setInitialState() {
-    if (this.window.navigator.userAgent && this.window.navigator.userAgent.indexOf('Googlebot/') !== -1) {
-      // Google Scholar requires article timeline to be open to improve indexing
-      this.$elm.dataset.initialState = 'opened';
-    } else if (this.doc.referrer && this.doc.referrer.indexOf('.google.') !== -1) {
-      // Google requires referrals to visually match the indexed version
-      this.$elm.dataset.initialState = 'opened';
-    } else if (this.viewportNoWiderThan(this.viewportWidthLarge)) {
-      // Force closed on small screens.
-      this.$elm.dataset.initialState = 'closed';
-    }
-
-    this.toggleContent();
-  }
-
   toggleContent() {
-    if (this.$elm.dataset.initialState === 'closed') {
-      this.$elm.classList.add('toggle--collapsed');
-      this.$toggle.classList.add('toggle--closed');
-    } else {
-      // Defensive: remove classes if they turn up here when they shouldn't
-      this.$toggle.classList.remove('toggle--closed');
-    }
-  }
-
-  initialiseOnResize() {
-    if (!this.viewportNoWiderThan(this.viewportWidthLarge)) {
-      return;
-    }
-
-    if (this.$toggle) {
-      if (this.viewportNoWiderThan(this.viewportWidthMedium) &&
-        this.listElements.length > this.maxVisibleCollapsedElementsOnSmallViewport &&
-        !this.$toggle.classList.contains('toggle--closed')) {
+    if (this.$elm.classList.contains('toggle--collapsed')) {
+      this.$elm.classList.remove('toggle--collapsed');
+      if (this.viewportNoWiderThan(this.viewportWidthMedium)) {
         this.alignToggle();
-      } else if (this.viewportNoWiderThan(this.viewportWidthLarge) &&
-        this.listElements.length > this.maxVisibleCollapsedElementsOnMediumViewport) {
-        this.$toggle.removeAttribute('style');
       }
+    } else {
+      this.$elm.classList.add('toggle--collapsed');
     }
   }
 
@@ -87,40 +49,9 @@ module.exports = class ContentAside {
     return this.window.matchMedia('(max-width: ' + thresholdInPx + 'px)').matches;
   }
 
-  toggleState(e) {
-    e.preventDefault();
-    if (this.$toggle.classList.contains('toggle--closed')) {
-      this.$elm.dispatchEvent(utils.eventCreator('expandsection'));
-    } else {
-      this.$elm.dispatchEvent(utils.eventCreator('collapsesection'));
-    }
-  }
-
   alignToggle() {
-    let lastElementsHeight = this.listElements.slice(this.listElements.length - 2);
-
-    let toggleHeight = 0;
-    lastElementsHeight.forEach(function (elem) {
-      toggleHeight += elem.offsetHeight;
-    });
-
-    this.$toggle.style.height = toggleHeight + 'px';
+    let elements = this.$elm.querySelectorAll('.definition-list--timeline dt');
+    let lastElement = elements[elements.length - 1];
+    this.$toggle.style.marginBottom = lastElement.offsetHeight + 'px';
   }
-
-  collapse() {
-    this.$toggle.classList.add('toggle--closed');
-    this.$elm.classList.add('toggle--collapsed');
-    this.$elm.dataset.initialState = 'closed';
-  }
-
-  expand() {
-    this.$toggle.classList.remove('toggle--closed');
-    this.$elm.classList.remove('toggle--collapsed');
-    this.$elm.dataset.initialState = 'opened';
-
-    if (this.viewportNoWiderThan(this.viewportWidthMedium)) {
-      this.alignToggle();
-    }
-  }
-
 };
