c77bafae0448a9d3c1ad8e4c1d80320823eca013
Viktoria Nedyalkova <viktoria.nedyalkova@endava.com>
2023-01-18 15:40:00 +0200

    Adjust JS logic to support two class names to trigger the Download popup

--- assets/js/components/ArticleDownloadLinksList.js
+++ assets/js/components/ArticleDownloadLinksList.js
@@ -14,12 +14,16 @@ module.exports = class ArticleDownloadLinksList {
     this.doc = doc;
     this.$elm = $elm;
 
-    this.$sideSectionWrapper = this.doc.querySelector('.side-section-wrapper__list');
-    if (!this.$sideSectionWrapper) {
+    this.$buttonWrapper = this.doc.querySelector('.side-section-wrapper__list') ||
+        this.doc.querySelector('.button-collection--inline');
+
+    if (!this.$buttonWrapper) {
       return;
     }
 
-    this.$downloadLink = this.$sideSectionWrapper.querySelector('.side-section-wrapper__download_link');
+    this.$downloadLink = this.$buttonWrapper.querySelector('.side-section-wrapper__download_link') ||
+        this.$buttonWrapper.querySelector('.button-download');
+
     if (!this.$downloadLink) {
       return;
     }
@@ -29,8 +33,10 @@ module.exports = class ArticleDownloadLinksList {
     this.$elm.classList.add('visuallyhidden');
     this.$elm.setAttribute('aria-expanded', 'false');
     this.hideJsExcludes();
+    this.$toggler = this.doc.querySelector('.side-section-wrapper__download_link') ||
+        this.doc.querySelector('.button-download');
+    
     this.moveList();
-    this.$toggler = this.doc.querySelector('.side-section-wrapper__download_link');
     this.$toggler.addEventListener('click', this.toggle.bind(this));
   }
 
@@ -44,12 +50,11 @@ module.exports = class ArticleDownloadLinksList {
   }
 
   /**
-   * Moves the download links list to be by the icon this.$toggler
+   * Moves the download links list to be under this.$toggler
    */
-  moveList() {
-    let $followingSibling = this.$downloadLink.nextElementSibling;
+    moveList() {
     this.$elm.parentNode.parentNode.classList.add('visuallyhidden');
-    this.$sideSectionWrapper.insertBefore(this.$elm, $followingSibling);
+    this.$toggler.after(this.$elm);
   }
 
   /**
