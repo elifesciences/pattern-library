6963ec745db3e0886e6dc0bd8e50443d40c27874
Viktoria Nedyalkova <viktoria.nedyalkova@endava.com>
2023-02-23 09:59:02 +0200

    JavaScript refactoring

--- test/contentaside.spec.js
+++ test/contentaside.spec.js
@@ -13,71 +13,19 @@ describe('A ContentAside Component', function () {
     $elm = document.querySelector('[data-behaviour="ContentAside"]');
   });
 
-  it('uses the "content-aside__sticky" css class', function () {
-    let contentAside = new ContentAside($elm);
-    expect(contentAside.cssStickyClassName).to.equal('content-aside__sticky');
+  it('has additional margin and padding when scroll bar is visible', function () {
+    let _contentAside = new ContentAside($elm);
+    let scrollbarWidth = 17;
+    _contentAside.addAdditionalSpaceIfScrollBarIsVisible(scrollbarWidth);
+    expect(_contentAside.$elm.style.marginRight).to.be.equal('-17px');
+    expect(_contentAside.$elm.style.paddingRight).to.be.equal('4px');
   });
 
-  it('adds and removes the "content-aside__sticky" class as the page is scrolled', function () {
-    let windowMock = {
-      addEventListener: function () {},
-      matchMedia: function() {
-        return {
-          matches: true
-        }
-      },
-    };
-
-    let _contentAside = new ContentAside($elm, windowMock);
-    expect(_contentAside.$elm.classList.contains('content-aside__sticky')).to.be.false;
-
-    // Note: The 'onScroll' handler is mocked, hence scroll and then manually call the handler.
-    window.scrollTo(0,1080);
-    _contentAside.handleScrolling();
-    expect(_contentAside.$elm.classList.contains('content-aside__sticky')).to.be.true;
-
-    // Note: The 'onScroll' handler is mocked, hence scroll and then manually call the handler.
-    window.scrollTo(0,0);
-    _contentAside.handleScrolling();
-    expect(_contentAside.$elm.classList.contains('content-aside__sticky')).to.be.false;
-  });
-
-  it('is removed when scrolling would cause content aside to overlay following layout elements', function () {
-    // This must be smaller than $elm.offsetHeight of the object under test
-    let fakeTopOfContentHeaderEl = -20;
-    let fakeBottomOfMainEl = 20;
-    let windowMock = {
-      addEventListener: function () {
-      },
-      matchMedia: function() {
-        return {
-          matches: true
-        }
-      }
-    };
-
-    let _contentAside = new ContentAside($elm, windowMock, document);
-    _contentAside.$contentHeader = {
-      getBoundingClientRect: function () {
-        return {
-          top: fakeTopOfContentHeaderEl
-        };
-      }
-    };
-    _contentAside.mainTarget = {
-      getBoundingClientRect: function () {
-        return {
-          bottom: fakeBottomOfMainEl
-        };
-      }
-    };
-    _contentAside.elmYOffset = 20;
-    // Prerequisite for the test to be valid
-    expect(fakeBottomOfMainEl).to.be.below(_contentAside.$elm.offsetHeight);
-
-    _contentAside.$elm.classList.add('content-aside__sticky');
-    _contentAside.handleScrolling();
-    expect(_contentAside.$elm.classList.contains('content-aside__sticky')).to.be.true;
-    expect(_contentAside.$elm.style.top.indexOf('px')).to.be.above(-1);
+    it('has zero margin and padding when scroll bar is not visible', function () {
+    let _contentAside = new ContentAside($elm);
+    let scrollbarWidth = 0;
+    _contentAside.addAdditionalSpaceIfScrollBarIsVisible(scrollbarWidth);
+    expect(_contentAside.$elm.style.marginRight).to.be.equal('0px');
+    expect(_contentAside.$elm.style.paddingRight).to.be.equal('0px');
   });
 });
