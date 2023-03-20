c969bf4cb4097edca1887b361e6c235beff07dbb
Viktoria Nedyalkova <viktoria.nedyalkova@endava.com>
2023-03-20 15:12:17 +0200

    Rename Tab link to Tab menu

--- assets/js/components/TabMenu.js
+++ assets/js/components/TabMenu.js
@@ -6,17 +6,17 @@ module.exports = class ContentAside {
     this.$elm = $elm;
     this.window = _window;
     this.doc = doc;
-    this.activeClassName = 'tab-link__active';
-    this.$tabLinksList = this.$elm.querySelector('.tab-link__list');
-    this.$tabLinksList.addEventListener('click', this.showActiveTab.bind(this));
+    this.activeClassName = 'tab-menu__active';
+    this.$tabMenuList = this.$elm.querySelector('.tab-menu__list');
+    this.$tabMenuList.addEventListener('click', this.showActiveTab.bind(this));
   }
 
   showActiveTab(e) {
     const lastActiveElement = this.$elm.querySelector('.' + this.activeClassName);
-    if (e.target.classList.contains('tab-link')) {
+    if (e.target.classList.contains('tab-menu')) {
       lastActiveElement.classList.remove(this.activeClassName);
       e.target.classList.add(this.activeClassName);
-    } else if (e.target.classList.contains('tab-link__long-name')) {
+    } else if (e.target.classList.contains('tab-menu__long-name')) {
       lastActiveElement.classList.remove(this.activeClassName);
       e.target.parentNode.classList.add(this.activeClassName);
     }
