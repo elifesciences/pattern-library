a136dabe5591c464481537f93369161cdd471a5b
Dave <d.price@elifesciences.org>
2024-11-05 11:28:58 +0000

    Add progressive enhancement for terms in eLife Assessment (#1328)
    * Add progressive enhancement for terms in eLife Assessment
    
    * refactor JS and css for progressive enhancement of Assessment

--- assets/js/components/Assessment.js
+++ assets/js/components/Assessment.js
@@ -13,20 +13,28 @@ module.exports = class Assessment {
     this.doc = doc;
     this.$elm = $elm;
     this.assessmentToggle();
+
   }
 
   assessmentToggle() {
     const assessment = this.$elm.querySelector('.assessment__container');
-    const button = this.$elm.querySelector('.assessment__toggle-btn');
+    const assessmentWrapper = document.querySelector('.assessment__wrapper');
+    const button = document.createElement('button');
+    button.classList.add('assessment__toggle-btn');
+    button.innerHTML = 'Read more about this assessment';
+    assessment.classList.add('hidden');
+    assessmentWrapper.appendChild(button);
 
     button.addEventListener('click', () => {
 
       if (assessment.getAttribute('aria-expanded') === 'false') {
         assessment.setAttribute('aria-expanded', 'true');
+        assessment.classList.remove('hidden');
         button.innerHTML = 'See less';
         button.classList.add('assessment__toggle-btn-reverse');
       } else {
         assessment.setAttribute('aria-expanded', 'false');
+        assessment.classList.add('hidden');
         button.innerHTML = 'Read more about this assessment';
         button.classList.remove('assessment__toggle-btn-reverse');
       }
